abd3d07199ff60319ae2afb0d84681f000cde9a9
Knouy <SanquirgoTanguy@Gmail.Com>
2020-05-03 18:54:10 +0200

    master

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    fetch: 'false',
+    jQuery: 'false',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+    "max-len": ["error", { "code": 80 }],
+    semi: ["error", "always"]
+  }
+}
